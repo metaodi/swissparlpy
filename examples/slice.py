350618224a0de7bfdd8b3b7b8c865a74237a4139
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2021-09-27 01:33:34 +0200

    Update example with smaller entity

--- examples/slice.py
+++ examples/slice.py
@@ -1,10 +1,15 @@
 import swissparlpy as spp
 from pprint import pprint
 
-subjects = spp.get_data(
-    table="Voting",
+sessions = spp.get_data(
+    table="Session",
 )
 
-print(f"Total rows: {len(subjects)}")
-for subject in subjects[5:10]:
-    pprint(subject)
+print(f"Total rows: {len(sessions)}")
+for session in sessions[5:10]:
+    pprint(session)
+
+
+# print any element
+print('')
+pprint(sessions[587])
