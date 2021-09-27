130d42be1386e8164eb3de476e0e61ff50d59529
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2021-09-27 11:13:49 +0200

    Simplify dependency management

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,7 +4,4 @@
 source pyenv/bin/activate
 
 pip install --upgrade pip
-pip install -r requirements.txt
-pip install -r test-requirements.txt
-pip install -r dev-requirements.txt
-pip install -e .
+flit install -s
--- setup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# shim used to be able to make a editable installation using `pip install -e .`
-
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup()
