13df11d146d7970f7ae7fc9263535f8d7d8113c8
Stefan Oderbolz <oderbolz@gmail.com>
2021-09-29 15:26:00 +0200

    Fix linting

--- examples/filter_advanced.py
+++ examples/filter_advanced.py
@@ -2,11 +2,13 @@ import swissparlpy as spp
 import pandas as pd
 
 
-def name_filter(e): 
+def name_filter(e):
     return spp.filter.or_(
         e.FirstName == 'Stefan',
         e.LastName == 'Seiler'
     )
+
+
 persons = spp.get_data("Person", filter=name_filter, Language='DE')
 
 df = pd.DataFrame(persons)
